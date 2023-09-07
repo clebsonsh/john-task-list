9c43412873b495232a6d4ee93faeb727de9521aa
Clebson Moura <clebson.sh@gmail.com>
2023-09-07 13:44:27 -0300

    route to the api

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', fn () => ['message' => config('app.name')]);
+Route::get('/', fn () => ['message' => config('app.name'), 'go to' => route('tasks.index')]);
